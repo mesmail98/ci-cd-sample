a8eba4b8aa8ad6817ee101ee58dd089f64622e25
mohamed esmail <mohamed.esmail@intel.com>
2023-04-06 09:28:01 -0700

    Adding hello program to test build automation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ci-cd-sample
+This is a sample project to create a buildable code through an automation YAML file
---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Avengers!" << endl;
+    cout << "My name is Jarvis, and I'll be your guide." << endl;
+    cout << "Let me introduce you to the team:" << endl;
+    cout << "Han Chau - Iron Man" << endl;
+    cout << "Kia Zandifar - Captain America" << endl;
+    cout << "Mohamed Esmail - Thor" << endl;
+    cout << "Together, they're the APPL SW Avengers" << endl;
+    return 0;
+}
+
